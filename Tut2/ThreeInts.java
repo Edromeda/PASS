8bda2da91e8cb4408b2a0f340308f32278a03cc2
Chris Edrom <Chrissarcovez@gmail.com>
2025-08-13 06:09:17 +0000

    Tutorial 3 Finished

--- Tut2/ThreeInts.java
+++ Tut2/ThreeInts.java
@@ -5,13 +5,13 @@ public class ThreeInts{
        public int c;
        
        public int middle() {
-        final int length = 3;
+        final int lgitength = 3;
         final int mid 1;
         int i = 0;
         int j;
         int[] array = {a, b, c};
 
         for (i < length; i++){
-            
+
         }
 }
---
+++ Tut3/Lab3.java
@@ -0,0 +1,17 @@
+/**
+ * KIT107 Lab 3 harness class
+ *
+ * @author Julian Dermoudy
+ * @version 8/8/2024
+ * 
+ * FILE IS COMPLETE
+ */
+public class Lab3 {
+    public static void main(String[] args) {
+        // Create application frame.
+        Lab3Frame frame = new Lab3Frame();
+        
+        // Show frame
+        frame.setVisible(true);
+    }
+}
